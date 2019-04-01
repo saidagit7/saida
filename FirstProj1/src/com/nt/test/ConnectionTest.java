49810a0dfd90f8e477b2c267f286fa908a48995a
Saida <Saida@DESKTOP-LM56POH>
2019-04-01 11:31:52 +0530

    de1-write comments

--- FirstProj1/src/com/nt/test/ConnectionTest.java
+++ FirstProj1/src/com/nt/test/ConnectionTest.java
@@ -6,8 +6,11 @@ import java.sql.DriverManager;
 public class ConnectionTest {
 	
 	public static void main(String []args) throws Exception {
+		Connection con=null;
+		
 		 Class.forName("oracle.jdbc.driver.OracleDriver");
-		 Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1522:orcl", "hr", "hr");
+		 //connection
+		 con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1522:orcl", "hr", "hr");
 		 if(con==null)
 			 System.out.println("connection is failed");
 		 else
